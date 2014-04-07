7a50fe582a120a96b6aa60fb336d8dfa6a908b77
keisec <crissg76@gmail.com>
2014-04-07 10:30:01 +0300

    The Begining
    YEAH

---
+++ Assets/Scripts/mapCreatorScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mapCreatorScript : MonoBehaviour {
+	public GameObject wall;
+	private int [,] mapExample=new int[,] {
+		{1,1,1,1,1,1,1,0,0,0,1,1,1,1,1},
+		{1,0,0,0,0,0,1,0,0,0,1,0,0,0,1},
+		{0,0,0,0,0,0,1,0,0,0,1,0,0,0,1},
+		{1,1,1,0,1,1,1,0,0,1,1,0,0,0,1},
+		{0,0,1,0,1,0,0,0,1,1,0,0,0,0,1},
+		{0,0,1,0,1,1,1,1,1,0,0,0,0,0,1},
+		{0,0,1,0,0,0,0,0,0,0,0,0,0,0,1},
+		{0,0,1,1,1,1,1,1,1,1,1,1,1,1,1}};
+	void Start () {
+		Vector2 position=new Vector2();
+		Quaternion rotation=new Quaternion();
+		for(int i=0;i<mapExample.GetLength(0);i++){
+			for(int j=0;j<mapExample.GetLength(1);j++){
+				if(mapExample[i,j]==1){
+					position.Set(0.5f+j,0.5f-i);
+					Instantiate(wall,position,rotation);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+	public float walkingSpeed;
+	private float moveVertical;
+	private float moveHorizontal;
+	private Vector2 movementVector=new Vector2();
+	void FixedUpdate(){
+		moveHorizontal=Input.GetAxis("Horizontal");
+		moveVertical=Input.GetAxis("Vertical");
+		movementVector.Set(moveHorizontal,moveVertical);
+		//movementVector.Scale(walkingSpeed);
+		rigidbody2D.velocity=movementVector*walkingSpeed;
+	}
+}
+/*using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public float tilt;
+	[System.Serializable]
+	public class Boundary{
+		public float xMin,xMax,zMin,zMax;
+	}
+	public Boundary boundary;
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire=0;
+	void Update(){
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire=Time.time+fireRate;
+			Instantiate(shot,shotSpawn.position,shotSpawn.rotation);
+			audio.Play();
+		}
+
+	}
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement=new Vector3(moveHorizontal,0.0f,moveVertical);
+		rigidbody.velocity = movement*speed;
+	
+		rigidbody.position = new Vector3 (
+			Mathf.Clamp(rigidbody.position.x,boundary.xMin,boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rigidbody.position.z,boundary.zMin,boundary.zMax)
+		);
+
+		rigidbody.rotation = Quaternion.Euler (0, 0, -tilt*rigidbody.velocity.x);
+	}
+}
+
+ */
+  
