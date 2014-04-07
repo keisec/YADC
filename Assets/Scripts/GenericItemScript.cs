680c72e1fa562ee12e0d2cc41ad5c0e27dff0d73
keisec <crissg76@gmail.com>
2014-04-07 14:06:55 +0300

    Adaugat baza

---
+++ Assets/Scripts/GenericItemScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericItemScript : MonoBehaviour {
+	public string itemName;
+	public string itemDescription;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public virtual void use(){
+
+	}
+	
+}
