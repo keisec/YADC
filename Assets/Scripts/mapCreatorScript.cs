e44c5419cc86b71ec0b3e7ee5dc50c8311e1d772
Roscaneanu George <zeroblitz36@gmail.com>
2014-04-07 11:02:52 +0300

    Added Dungeon Tiles

--- Assets/Scripts/mapCreatorScript.cs
+++ Assets/Scripts/mapCreatorScript.cs
@@ -3,23 +3,26 @@ using System.Collections;
 
 public class mapCreatorScript : MonoBehaviour {
 	public GameObject wall;
+	public GameObject tile;
 	private int [,] mapExample=new int[,] {
 		{1,1,1,1,1,1,1,0,0,0,1,1,1,1,1},
-		{1,0,0,0,0,0,1,0,0,0,1,0,0,0,1},
-		{0,0,0,0,0,0,1,0,0,0,1,0,0,0,1},
-		{1,1,1,0,1,1,1,0,0,1,1,0,0,0,1},
-		{0,0,1,0,1,0,0,0,1,1,0,0,0,0,1},
-		{0,0,1,0,1,1,1,1,1,0,0,0,0,0,1},
-		{0,0,1,0,0,0,0,0,0,0,0,0,0,0,1},
+		{1,2,2,2,2,2,1,0,0,0,1,2,2,2,1},
+		{2,2,2,2,2,2,1,0,0,0,1,2,2,2,1},
+		{1,1,1,2,1,1,1,0,0,1,1,2,2,2,1},
+		{0,0,1,2,1,0,0,0,1,1,2,2,2,2,1},
+		{0,0,1,2,1,1,1,1,1,2,2,2,2,2,1},
+		{0,0,1,2,2,2,2,2,2,2,2,2,2,2,1},
 		{0,0,1,1,1,1,1,1,1,1,1,1,1,1,1}};
 	void Start () {
 		Vector2 position=new Vector2();
 		Quaternion rotation=new Quaternion();
 		for(int i=0;i<mapExample.GetLength(0);i++){
 			for(int j=0;j<mapExample.GetLength(1);j++){
+				position.Set(0.5f+j,0.5f-i);
 				if(mapExample[i,j]==1){
-					position.Set(0.5f+j,0.5f-i);
 					Instantiate(wall,position,rotation);
+				}else if(mapExample[i,j]==2){
+					Instantiate(tile,position,rotation);
 				}
 			}
 		}
