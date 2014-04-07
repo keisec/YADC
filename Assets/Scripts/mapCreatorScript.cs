f987d5655ea30092d6af16119fe7b975c614b4d3
Roscaneanu George <zeroblitz36@gmail.com>
2014-04-07 14:24:16 +0300

    Added Shots

--- Assets/Scripts/mapCreatorScript.cs
+++ Assets/Scripts/mapCreatorScript.cs
@@ -14,11 +14,11 @@ public class mapCreatorScript : MonoBehaviour {
 		{0,0,1,2,2,2,2,2,2,2,2,2,2,2,1},
 		{0,0,1,1,1,1,1,1,1,1,1,1,1,1,1}};
 	void Start () {
-		Vector2 position=new Vector2();
+		Vector3 position=new Vector3();
 		Quaternion rotation=new Quaternion();
 		for(int i=0;i<mapExample.GetLength(0);i++){
 			for(int j=0;j<mapExample.GetLength(1);j++){
-				position.Set(0.5f+j,0.5f-i);
+				position.Set(0.5f+j,0.5f-i,2);
 				if(mapExample[i,j]==1){
 					Instantiate(wall,position,rotation);
 				}else if(mapExample[i,j]==2){
--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -4,16 +4,52 @@ using System.Collections;
 public class playerScript : MonoBehaviour {
 	public float walkingSpeed;
 	public float maximumHP;
+
 	private float currentHP;
 	private float moveVertical;
 	private float moveHorizontal;
 	private Vector2 movementVector=new Vector2();
 	void FixedUpdate(){
-		moveHorizontal=Input.GetAxis("Horizontal");
-		moveVertical=Input.GetAxis("Vertical");
+		moveHorizontal=0;
+		moveVertical=0;
+		if(Input.GetKey("a"))moveHorizontal-=1;
+		if(Input.GetKey("d"))moveHorizontal+=1;
+		if(Input.GetKey("w"))moveVertical+=1;
+		if(Input.GetKey("s"))moveVertical-=1;
+		//moveHorizontal=Input.GetAxis("Horizontal");
+		//moveVertical=Input.GetAxis("Vertical");
 		movementVector.Set(moveHorizontal,moveVertical);
 		//movementVector.Scale(walkingSpeed);
 		rigidbody2D.velocity=movementVector*walkingSpeed;
+
+		CheckMouseClick();
+	}
+	public float fireRate;
+	public GameObject bulletObject;
+	private float nextFire=0;
+	public float bulletSpeed;
+	private Vector3 mouse_pos;
+	private Vector3 object_pos;
+	private float angle;
+	void CheckMouseClick(){
+		mouse_pos = Input.mousePosition;
+		mouse_pos.z = 0.0f; 
+		object_pos = Camera.main.WorldToScreenPoint(transform.position);
+		mouse_pos.x = mouse_pos.x - object_pos.x;
+		mouse_pos.y = mouse_pos.y - object_pos.y;
+		angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+		Vector3 rotationVector = new Vector3 (0, 0, angle);
+		transform.rotation = Quaternion.Euler(rotationVector);
+		if(Input.GetMouseButton(0)&&Time.time>nextFire){
+			nextFire=Time.time+fireRate;
+			GameObject bullet = (GameObject)Instantiate(bulletObject, transform.position+Vector3.Normalize(mouse_pos), transform.rotation);
+			//bullet.transform.rotation.SetFromToRotation(transform.position,mouse_pos);
+
+			//bullet.rigidbody2D.velocity.Set(Mathf.Cos(angle)*bulletSpeed,Mathf.Sin(angle)*bulletSpeed);
+			bullet.rigidbody2D.AddForce(bullet.transform.right*100*bulletSpeed);
+			//bullet.rigidbody2D.velocity.Set(10,10);
+			//bullet.rigidbody2D.AddForce(Vector2(Mathf.Cos(angle),Mathf.Sin(angle))*bulletSpeed);
+		}
 	}
 }
 /*using UnityEngine;
