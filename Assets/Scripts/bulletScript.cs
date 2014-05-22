e1e7be4be35370342635b9fd1e460107737aeaf9
dayNightingale <alina.b144@yahoo.com>
2014-05-22 14:59:37 +0300

    Merge branch 'master' of https://github.com/keisec/YADC
    Conflicts:
    	Assets/Scenes/mainScene.unity
    	Assets/Scripts/mapCreatorScript.cs
    	Assets/Textures/light staff.png.meta
    	ProjectSettings/EditorBuildSettings.asset
    	ProjectSettings/Physics2DSettings.asset
    	ProjectSettings/TagManager.asset

---
+++ Assets/Scripts/bulletScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletScript : MonoBehaviour {
+	void OnCollisionEnter2D(Collision2D other){
+		//Debug.Log("Collision at "+other.transform.position.ToString());
+		if(other.gameObject.tag=="Wall"){
+			Destroy(gameObject);
+		}
+	}
+	void Update() {
+		if(transform.position.magnitude>200){
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/mainGuiScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainGuiScript : MonoBehaviour {
+	public string connectionIP="localhost";
+	public int portNumber=8632;
+	private bool connected=false;
+	private void OnConnectedToServer(){
+		//A client has just connected
+		Debug.Log ("Connected To Server");
+		connected=true;
+		//Application.LoadLevel("mainScene");
+	}
+	private void OnServerInitialized(){
+		//The server has initialized
+		connected=true;
+		//Application.LoadLevel("mainScene");
+	}
+	private void OnDisconnectedFromServer(){
+		//The connection has been lost or disconnected
+		connected=false;
+	}
+	void OnFailedToConnect(NetworkConnectionError error) {
+		Debug.Log("Could not connect to server: " + error);
+	}
+	void OnDisconnectedFromServer(NetworkDisconnection info) {
+		if (Network.isServer)
+			Debug.Log("Local server connection disconnected");
+		else
+			if (info == NetworkDisconnection.LostConnection)
+				Debug.Log("Lost connection to the server");
+		else
+			Debug.Log("Successfully diconnected from the server");
+	}
+	private void OnGUI(){
+		if(!connected){
+			connectionIP=GUILayout.TextField(connectionIP);
+			int.TryParse(GUILayout.TextField(portNumber.ToString()),out portNumber);
+			if (GUILayout.Button("Connect")){
+				Debug.Log("Attepting to connect");
+				Network.Connect(connectionIP,portNumber);
+			}
+			if(GUILayout.Button("Host"))
+				Network.InitializeServer(4,portNumber,false);
+		}
+		else{
+			GUILayout.Label("Connections: "+Network.connections.Length.ToString());
+			if(GUILayout.Button("Disconnect")){
+				Network.Disconnect();
+			}
+		}
+	}
+}
