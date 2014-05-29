9b108acd57a247fd11d12c3e86d57ea681a65091
lordaeron92 <lordaeron92@gmail.com>
2014-05-29 18:59:04 +0300

    commit

---
+++ Assets/Scripts/PopupListClasa.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopupListClasa : MonoBehaviour {
+	private bool showList= false;
+	private int listEntry= 0;
+	private GUIContent[] list;
+	private GUIStyle listStyle;
+	private bool picked= false;
+	public int pozx=0;
+	public int pozy=0;
+	void  Start (){
+		// Make some content for the popup list
+		list = new GUIContent[5];
+		list[0] = new GUIContent("Warrior");
+		list[1] = new GUIContent("Wizard");
+		list[2] = new GUIContent("Rogue");
+		list[3] = new GUIContent("Bard");
+
+		
+		// Make a GUIStyle that has a solid white hover/onHover background to indicate highlighted items
+		listStyle = new GUIStyle();
+		listStyle.normal.textColor = Color.white;
+		Texture2D tex= new Texture2D(2, 2);
+		Color[] colors= new Color[4];
+		 
+		for(int i=0;i<4;i++)colors[i]=Color.white;
+		tex.SetPixels(colors);
+		tex.Apply();
+		listStyle.hover.background = tex;
+		listStyle.onHover.background = tex;
+		listStyle.padding.left = listStyle.padding.right = listStyle.padding.top = listStyle.padding.bottom = 4;
+	}
+	
+	void  OnGUI (){
+		if (Popup.List ( new Rect(pozx, pozy, 200, 50),ref showList,ref listEntry,new GUIContent(list[listEntry].text), list, listStyle)) {
+			picked = true;
+		}
+
+
+	}
+	void Update() {
+		 pozx=(Screen.width/2)-100;
+		pozy = ( 2 * Screen.height / 3) +50;
+	}
+}
