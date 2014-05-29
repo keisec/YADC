96840dc7ae1359d0e88495bea3067336c7dc1007
keisec <crissg76@gmail.com>
2014-05-29 11:06:04 +0300

    commit

---
+++ Assets/Scripts/DataBaseScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Data.SQLite;
+
+public static class DataBaseScript {
+
+	// Use this for initialization
+
+	public static SQLiteConnection connection;
+
+	public static void init(string conectionString)
+	{
+		connection = new SQLiteConnection (conectionString);
+	}
+
+	public static void addUser(
+}
