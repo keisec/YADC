4e1125f92c8e9a197787a97751c2be3beb8a8fd8
keisec <crissg76@gmail.com>
2014-05-30 22:28:51 +0300

    commit

--- Assets/Scripts/DatabaseAccessScript.cs
+++ Assets/Scripts/DatabaseAccessScript.cs
@@ -5,12 +5,19 @@ using Mono.Data.SqliteClient;
 public class DatabaseAccessScript : MonoBehaviour {
 
 	public SqliteConnection sqlcon;
-	public string connectionString;
+	public string DatabaseName;
 
-	DatabaseAccessScript(){
-		sqlcon = new SqliteConnection (connectionString);
-		sqlcon.Open ();
+	void Start(){
+		//SqliteConnectionStringBuilder conn_builder = new SqliteConnectionStringBuilder ();
+
+		sqlcon = new SqliteConnection ("Uri="+@DatabaseName);
+		sqlcon.Open ();	
+		SqliteCommand sqlcomm = new SqliteCommand ("Select username,password from utilizator;",sqlcon);
 		Debug.Log ("1");
+		SqliteDataReader sdr = sqlcomm.ExecuteReader ();
+		while (sdr.Read()) {
+			Debug.Log(sdr.GetString(1)+sdr.GetString(2));
+		}
 	}
 
 	// Update is called once per frame
