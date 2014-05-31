005dd184ec7a1b318f7108c48ff55b89b05395cd
keisec <crissg76@gmail.com>
2014-05-31 12:15:44 +0300

    commit

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -5,7 +5,8 @@ public class Login : MonoBehaviour {
 	public string username="username";
 	public string pass="password";
 	public GameObject dataBase;
-	public string eroare="";
+	public string eroare="eroare";
+	private string errorMessage="";
 	// Use this for initialization
 	void Start () {
 	
@@ -16,8 +17,8 @@ public class Login : MonoBehaviour {
 	
 	}
 	private void OnGUI(){
-		GUI.Label(new Rect(Screen.width/2-100, ( 2 * Screen.height / 3)-50, 200, 50), eroare);
-			username=GUI.TextField( new Rect(Screen.width/2-100,( 2 * Screen.height / 3)-200,200,50),username);
+		GUI.Label(new Rect(Screen.width/2-100, ( 2 * Screen.height / 3)-50, 200, 50), errorMessage);
+		username=GUI.TextField( new Rect(Screen.width/2-100,( 2 * Screen.height / 3)-200,200,50),username);
 		pass=GUI.PasswordField( new Rect(Screen.width/2-100,( 2 * Screen.height / 3)-150,200,50),pass,"*"[0]);
 
 		if (GUI.Button (new Rect (Screen.width/2-100,( 2 * Screen.height / 3)-100,200,50),"Login"))
@@ -28,7 +29,7 @@ public class Login : MonoBehaviour {
 			}
 			else
 			{
-				eroare="eroare";
+				errorMessage=eroare;
 			}
 
 		}
